3cfa243c9b0923adfbce6c7cbde1d594686f027c
Dayna <simru0411@gmail.com>
2025-08-18 17:56:56 +0800

    Create Week 2 Lecture.C

---
+++ Week 2 Lecture.C
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdbool.h> //introduces boolean type true or false
+#include <math.h> //math library
+
+//Structures -> used to combine related data to a var
+
+struct Position { //good practice to Capitalise structs to know that it is a type
+  int x;
+  int y;
+}
+
+//alternative declaration to reduce typing
+
+struct typedef {
+  int x;
+  int y;
+} Position;
+
+int main(void){
+  Position a;
+  a.x = 5;
+  a.y = 10;
+
+  Position b = {.x =5, .y = 10}
+}
+
+//Conditionals
+
+if (<expression>) {
+  //IMPORTANT !!! in C, true: anything that is not equal to 0
+  //statements to execute
+}
+
+else {
+  //statements to execute when if-condition is not met
+}
+
+//logical operators
+// $$ -> AND
+// || -> OR
+// == -> EQUAL
+// != -> NOT EQUAL
+
+//DOUBLE VALUES CANNOT BE USED TO DO COMPARISON
+
+//How to use Floating-Point Equality
+
+int main(void){
+  a = 0.1 + 0.2;
+  b = 0.3;
+
+  if (fabs(a-b), e-5){
+    //execute statement
+  }
+
+}
